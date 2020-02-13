8a7a59f39f68171dfe4098995223fe7f92a7ea1f
sunshine <tajohnsonn@gmail.com>
2020-02-13 15:36:40 -0800

    updating js

--- mysrc.js
+++ mysrc.js
@@ -1,7 +1,3 @@
-/////
-// variables
-/////
-
 // calling in moment to be used in several places
 const m = moment();
 
@@ -9,37 +5,13 @@ const m = moment();
 var words;
 var hourInfo;
 
-// variables for color coding
+// time display
 
-/////
-// display
-/////
-
-// Near the top of the calendar, the application should display the current day.
-// moment(specific format that we desire)
-// grab local date of the users machine, and spit it out in the
-// format that we desire
 console.log(m.format("dddd, MMMM Do"));
 
 $("#currentDay").text(m.format("dddd, MMMM Do"));
 
-/////
-// functions
-/////
-
-// each hour should be color coded to reflect whether the time slot is in the past, the present,
-// or the future. This will change depending on the time of day.
-// * App is aware of what time it is
-// * if line is current hour it is green and the line after it is purple
-// * else it is a light lavendar
-
-// if that knows what the current time is and highlights the current hour
-// if current hour, else if current hour +1, else past hour color
-// is ran on each page refresh and each save?
-
-// THIS FUNCTION IS NOT WORKING THE WAY I THOUGHT IT WOULD
-// ADDITIONALLY IT NEEDS TO BE CALLED ON DOCUMENT LOAD AND
-// ALSO BE AVAILABLE TO BE CALLED ON SAVE
+// color change by the hour
 
 $(document).ready(function() {
   colorChange();
@@ -51,8 +23,6 @@ function colorChange() {
   // console.log("Time test" + timeTest);
   console.log("Current Time" + realTime);
 
-  //for each of the hours unique positions apply below
-  // right now it's applying to all
   $(".input").each(function() {
     var timeTest = parseInt($(this).attr("id"));
     console.log(timeTest);
@@ -73,17 +43,7 @@ function colorChange() {
   });
 }
 
-// var saveWords = JSON.parse(localStorage.getItem("9 am"));
-// $("#nineAm").val("");
-// $("#nineAm").val(saveWords);
-// dummy function that then is called by the first dummy function
-// and prints the localStorage
-// informaiton to the page
-
-// function that listen for clicks, knows which row the button is connected
-// to, and then runs the dummy function to save the information
-// dummy function that allows us to save that row to localStorag
-// happens on click of any .saveBtn
+//  save button
 $(".saveBtn").click(function() {
   words = $(this)
     .siblings(".input")
@@ -99,12 +59,8 @@ $(".saveBtn").click(function() {
   renderText();
 });
 
-// FUNCTION SAVEWORDS
-// Need to change these into a dummy function that refreshes each
-// slot with it's currently stored information
+// saving input to local storage
 
-// couldn't for the life of me wrap my head around how to make this
-// a dummy function :(
 function renderText() {
   var saveWords9 = JSON.parse(localStorage.getItem("9:00 am"));
   $("#9").val("");
